6e271c4b440cc8c43074ae4b9754ba37d5b6cac8
Ana Doig <ana.doig@bootcamp-upgrade.com>
2022-06-26 22:47:25 +0200

    Adding navigation + hero

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ManagementComponent } from './pages/management/management.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "management", component: ManagementComponent
+  },
+  {
+    path: "", pathMatch: "full", component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
